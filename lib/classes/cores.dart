061180f64da4c60ac8b36836afbe1493e9143f2d
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2024-08-01 17:39:58 -0300

    Adicionado novos assets ao projeto - 2024.8.1.2

--- lib/classes/cores.dart
+++ lib/classes/cores.dart
@@ -6,10 +6,13 @@ class Cores {
   // static const Color marromMedio = Color(0xFFCD853F);
   // static const Color marrom = Color(0xFF800000);
   static const Color marromEscuro = Color(0xFF46220F);
-  static const Color marromMedio = Color(0xFF704214);
+  // static const Color marromMedio = Color(0xFF704214);
+  static const Color marromMedio = Color(0xFF996A34);
   static const Color marrom = Color(0xFFAA7444);
+  static const Color verde = Color(0xFF2E7D32);
   // static const Color marrom = Color(0xFFc69e60);
 
   static const Color branco = Color(0xFFFFFFFF);
   static const Color preto = Color(0xFF000000);
+  static const Color cinza = Color(0xFF808080);
 }
