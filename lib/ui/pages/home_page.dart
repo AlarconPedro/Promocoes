5b9dafffaa3cc3e9adfaa1557672b1cd4660fc52
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2024-08-01 17:40:49 -0300

    Alterado esquema de layout principal para melhor alocar as informações em tela - 2024.8.1.3

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:promocoes/classes/classes.dart';
 import 'package:promocoes/classes/layouts.dart';
+import 'package:promocoes/ui/pages/cadastro_participantes_page.dart';
 import 'package:promocoes/ui/widgets/botao_container.dart';
 
 class HomePage extends StatefulWidget {
@@ -47,109 +48,71 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     calcularTamanhoTela();
     return Scaffold(
-      backgroundColor: Cores.marromMedio,
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              color: Cores.marrom,
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 50),
-                    child: Text(
-                      'Promoção',
-                      style: TextStyle(
-                        color: Cores.branco,
-                        fontSize: 30,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Cores.marrom,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.asset(
-                          'images/baner.jpeg',
-                          fit: BoxFit.cover,
+      backgroundColor: Cores.branco,
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  color: Cores.branco,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 10, right: 10, bottom: 10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Cores.marrom,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ClipRRect(
+                            // borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              'images/banner_grande.png',
+                              fit: BoxFit.fitHeight,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: largura > 700
-                      ? Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            BotaoContainer(
-                              icone: CupertinoIcons.person_add_solid,
-                              titulo: 'Cadastrar-se',
-                              layout: layout,
-                              onTap: () {
-                                print('Clicou');
-                              },
-                            ),
-                            BotaoContainer(
-                              icone: CupertinoIcons.tickets_fill,
-                              titulo: 'Novo Cupom',
-                              layout: layout,
-                              onTap: () {
-                                print('Clicou');
-                              },
-                            ),
-                            BotaoContainer(
-                              icone: CupertinoIcons.gift_fill,
-                              titulo: 'Sorteios',
-                              layout: layout,
-                              onTap: () {
-                                print('Clicou');
-                              },
-                            ),
-                          ],
-                        )
-                      : Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(4),
-                              child: BotaoContainer(
-                                icone: CupertinoIcons.person_add_solid,
-                                titulo: 'Cadastrar-se',
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  color: Cores.marromMedio,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: largura > 700
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              BotaoContainer(
+                                icone: CupertinoIcons.add_circled_solid,
+                                titulo: 'Cadastrar Cupom',
                                 layout: layout,
                                 onTap: () {
-                                  print('Clicou');
+                                  Navigator.push(
+                                    context,
+                                    CupertinoDialogRoute(
+                                      builder: (context) =>
+                                          const CadastroParticipantes(),
+                                      context: context,
+                                    ),
+                                  );
                                 },
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(4),
-                              child: BotaoContainer(
+                              BotaoContainer(
                                 icone: CupertinoIcons.tickets_fill,
-                                titulo: 'Novo Cupom',
+                                titulo: 'Meus Cupons',
                                 layout: layout,
                                 onTap: () {
                                   print('Clicou');
                                 },
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(4),
-                              child: BotaoContainer(
+                              BotaoContainer(
                                 icone: CupertinoIcons.gift_fill,
                                 titulo: 'Sorteios',
                                 layout: layout,
@@ -157,32 +120,79 @@ class _HomePageState extends State<HomePage> {
                                   print('Clicou');
                                 },
                               ),
-                            ),
-                          ],
-                        ),
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  height: 80,
-                  width: MediaQuery.of(context).size.width,
-                  color: Cores.marrom,
-                  child: const Center(
-                    child: Text(
-                      'Rodapé',
-                      style: TextStyle(
-                        color: Cores.branco,
-                        fontSize: 30,
-                      ),
-                    ),
+                            ],
+                          )
+                        : Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: BotaoContainer(
+                                  icone: CupertinoIcons.add_circled_solid,
+                                  titulo: 'Cadastrar Cupom',
+                                  layout: layout,
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      CupertinoDialogRoute(
+                                        builder: (context) =>
+                                            const CadastroParticipantes(),
+                                        context: context,
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: BotaoContainer(
+                                  icone: CupertinoIcons.tickets_fill,
+                                  titulo: 'Meus Cupons',
+                                  layout: layout,
+                                  onTap: () {
+                                    print('Clicou');
+                                  },
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: BotaoContainer(
+                                  icone: CupertinoIcons.gift_fill,
+                                  titulo: 'Sorteios',
+                                  layout: layout,
+                                  onTap: () {
+                                    print('Clicou');
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
                   ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          const Spacer(),
+          Row(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.06,
+                width: MediaQuery.of(context).size.width,
+                color: Cores.marromMedio,
+                child: Center(
+                  child: Text(
+                    'Rodapé',
+                    style: TextStyle(
+                      color: Cores.preto,
+                      fontWeight: FontWeight.bold,
+                      fontSize: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
