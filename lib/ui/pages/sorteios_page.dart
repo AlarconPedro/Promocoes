3f1f24f10ade0ede04bc61efe0afeecf38c4aaff
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2024-08-02 17:36:10 -0300

    Criado tela base de listagem de sorteios - 2024.8.2.2

---
+++ lib/ui/pages/sorteios_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import '../../classes/classes.dart';
+
+class SorteiosPage extends StatefulWidget {
+  Function onClique;
+
+  SorteiosPage({super.key, required this.onClique});
+
+  @override
+  State<SorteiosPage> createState() => _SorteiosPageState();
+}
+
+class _SorteiosPageState extends State<SorteiosPage> {
+  double altura = 650;
+  double largura = 650;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Center(
+          child: Flexible(
+            fit: FlexFit.loose,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              width: largura,
+              height: altura,
+              decoration: BoxDecoration(
+                color: Cores.branco,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Cores.cinza,
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                    child: Text(
+                      'Sorteios',
+                      style: TextStyle(
+                        color: Cores.preto,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: 10,
+                        itemBuilder: (context, index) {
+                          return const ListTile(
+                            title: Text(
+                                'Sorteio: item | Ocorrido: 00/00/2024 | Ganhador: Fulano de Tal'),
+                          );
+                        }),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
